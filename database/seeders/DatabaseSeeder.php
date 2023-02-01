b9cb70a1c12b766c786f97751741230cfe74dd6f
Biljana Novakovic <novakovic1701@gmail.com>
2023-02-01 13:53:55 +0100

    Napravljena laravel aplikacija

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /* $this->call(Vrste::class);
+        $this->call(Klijenti::class);
+        $this->call(Termini::class);
+        $this->call(Users::class); */
+    }
+}
