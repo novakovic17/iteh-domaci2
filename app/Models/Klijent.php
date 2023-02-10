bf1f39713691f154ee345723e2af01714dd289e0
Biljana Novakovic <novakovic1701@gmail.com>
2023-02-10 12:03:32 +0100

    Uneti modeli

---
+++ app/Models/Klijent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Klijent extends Model
+{
+    use HasFactory;
+
+    protected $table = 'klijenti';
+
+    protected $fillable = ['ime', 'prezime'];
+}
